2734d0cba39d553f90a9fcf4a60076798a5c4b19
Pratik-Guha <sguha4966@gmail.com>
2023-11-01 12:59:24 +0530

    updated

--- script.js
+++ script.js
@@ -167,13 +167,22 @@ sr.reveal(`.about_img-overlay,.video_content,.subscribe_form`,{
 })
 
 // <========= Registration Part ===========>
-const user_con=document.querySelector('.user-container');
-const regis_btn=document.querySelector('.registration-btn');
-const login_btn=document.querySelector('.login-btn');
+// const user_con=document.querySelector('.user-container');
+// const regis_btn=document.querySelector('.registration-btn');
+// const login_btn=document.querySelector('.login-btn');
 
-regis_btn.addEventListener('click',()=>{
-    user_con.classList.add('login-section--display')
-})
-login_btn.addEventListener('click',()=>{
-    user_con.classList.remove('login-section--display')
-})
+// regis_btn.addEventListener('click',()=>{
+//     user_con.classList.add('login-section--display')
+// })
+// login_btn.addEventListener('click',()=>{
+//     user_con.classList.remove('login-section--display')
+// })
+function showLogin() {
+    const userContainer = document.getElementById('userContainer');
+    userContainer.classList.remove('login-section--display');
+}
+
+function showRegistration() {
+    const userContainer = document.getElementById('userContainer');
+    userContainer.classList.add('login-section--display');
+}
