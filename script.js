88572e5d45d1f71f5984798ea5a899e69828f9d6
Pratik-Guha <sguha4966@gmail.com>
2023-11-01 11:53:20 +0530

    updated

--- script.js
+++ script.js
@@ -165,3 +165,15 @@ sr.reveal(`.about_img-overlay,.video_content,.subscribe_form`,{
     origin:'right',
     interval:100,
 })
+
+// <========= Registration Part ===========>
+const user_con=document.querySelector('.user-container');
+const regis_btn=document.querySelector('.registration-btn');
+const login_btn=document.querySelector('.login-btn');
+
+regis_btn.addEventListener('click',()=>{
+    user_con.classList.add('login-section--display')
+})
+login_btn.addEventListener('click',()=>{
+    user_con.classList.remove('login-section--display')
+})
