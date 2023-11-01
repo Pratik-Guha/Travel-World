a36de217861c87662db10fa56c7217ff13a7749d
Pratik-Guha <sguha4966@gmail.com>
2023-11-01 23:18:50 +0530

    updated

--- script.js
+++ script.js
@@ -167,16 +167,7 @@ sr.reveal(`.about_img-overlay,.video_content,.subscribe_form`,{
 })
 
 // <========= Registration Part ===========>
-// const user_con=document.querySelector('.user-container');
-// const regis_btn=document.querySelector('.registration-btn');
-// const login_btn=document.querySelector('.login-btn');
 
-// regis_btn.addEventListener('click',()=>{
-//     user_con.classList.add('login-section--display')
-// })
-// login_btn.addEventListener('click',()=>{
-//     user_con.classList.remove('login-section--display')
-// })
 function showLogin() {
     const userContainer = document.getElementById('userContainer');
     userContainer.classList.remove('login-section--display');
@@ -185,4 +176,14 @@ function showLogin() {
 function showRegistration() {
     const userContainer = document.getElementById('userContainer');
     userContainer.classList.add('login-section--display');
+}
+// <================= Search portion ================>
+const priceRange = document.getElementById("priceRange");
+const priceAmount = document.getElementById("priceAmount");
+
+priceRange.addEventListener("input", updatePrice);
+
+function updatePrice() {
+  const selectedPrice = priceRange.value;
+  priceAmount.textContent = `$${selectedPrice}`;
 }
